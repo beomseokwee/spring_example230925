de796a804f6547398c7d4a1834b042e88d7a3622
beomseokwee <wijang777@naver.com>
2023-09-26 21:56:04 +0900

    에러 미해결

--- src/main/java/com/example/lesson03/mapper/ReviewMapper.java
+++ src/main/java/com/example/lesson03/mapper/ReviewMapper.java
@@ -25,4 +25,13 @@ public interface ReviewMapper {
 			@Param("point") Double point,
 			@Param("review") String review
 			);
+	
+	
+	public int updateReviewById(
+			@Param("id") int id,
+			@Param("review") String review);
+	
+	public int deleteReviewById(int id); 
 }
+
+	
---
+++ src/main/java/com/example/lesson04/bo/UserBO.java
@@ -0,0 +1,26 @@
+package com.example.lesson04.bo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.lesson04.domain.User;
+import com.example.lesson04.mapper.UserMapper;
+
+@Service
+public class UserBO {
+	//input : 컬럼들 4개
+	// output: X
+	
+	@Autowired
+	private UserMapper userMapper;
+	
+	public void addUser(String name, String yyyymmdd, String email, String introduce) {
+		
+		
+		userMapper.insertUser(name, yyyymmdd, email, introduce);
+	}
+	
+	public User getLatestUser() {
+		return userMapper.selectLatestUser();
+	}
+}
